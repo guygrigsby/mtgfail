809177d86bfb370b01ea961319368842f305fe7b
Guy J Grigsby <guy.grigsby@gmail.com>
2021-03-06 22:28:53 -0700

    working decode

--- tabletopsimulator/decode.go
+++ tabletopsimulator/decode.go
@@ -1,8 +1,45 @@
 package tabletopsimulator
 
-import "io"
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
 
-func Decode(ttsFormat io.Reader) (io.Reader, error) {
+	"github.com/guygrigsby/mtgfail"
+	"github.com/inconshreveable/log15"
+)
 
-	return nil, nil
+func Decode(ttsFormat io.Reader, log log15.Logger) ([]*mtgfail.Entry, error) {
+	b, err := ioutil.ReadAll(ttsFormat)
+	if err != nil {
+
+		log.Error(
+			"Cannot read deckfile",
+			"err", err,
+		)
+		return nil, err
+	}
+	var deckfile []ObjectState
+	err = json.Unmarshal(b, &deckfile)
+	if err != nil {
+		log.Error(
+			"Cannt unmarshal tts deckfile",
+			"err", err,
+		)
+		return nil, err
+	}
+
+	var deck []*mtgfail.Entry
+
+	for i, entry := range deckfile {
+
+		deck = append(deck, &mtgfail.Entry{
+			Name: entry.Nickname,
+			ImageUris: mtgfail.ImageUris{
+				Normal: entry.CustomDeck[i+1].FaceURL,
+			},
+		})
+	}
+
+	return deck, nil
 }
