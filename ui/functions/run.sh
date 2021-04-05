9fe945944c5a1589f2ae339940bec4902bff7cf5
Guy J Grigsby <guy.grigsby@gmail.com>
2021-04-05 10:22:02 -0600

    remove extra crap

--- ui/functions/run.sh
+++ ui/functions/run.sh
@@ -1,5 +1,4 @@
 #!/bin/zsh
 		
 FIREBASE_CONFIG=`cat ~/marketplace-c87d0-firebase-adminsdk-3gz7a-d0d7f659a0.json`
-export FIREBASE_CONFIG
-go run cmd/local/main.go
+FIREBASE_CONFIG=$FIREBASE_CONFIG go run cmd/local/main.go
